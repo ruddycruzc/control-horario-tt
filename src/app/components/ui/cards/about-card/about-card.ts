1317a28799fed036665fb03dbc7c951c56b2a9c0
Seni99 <inesarriazu@gmail.com>
2026-06-18 13:22:23 +0200

    feat: update hero jornada section styles and countdown logic; remove unused about card component

--- src/app/components/ui/cards/about-card/about-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ayuda-card',
-  imports: [],
-  templateUrl: './ayuda-card.html',
-  styleUrl: './ayuda-card.css',
-})
-export class AyudaCard {}
